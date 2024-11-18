c2258d8626266c717349d4acbfac4feb0b32751a
Dr. K.D. Murray <foss@kdmurray.id.au>
2024-11-18 11:08:27 +0100

    adopt scm

--- ot2ools/__init__.py
+++ ot2ools/__init__.py
@@ -5,6 +5,9 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from ._version import version
+__version__ = version
+
 from sys import argv, exit, stderr
 cmds = {}
 
